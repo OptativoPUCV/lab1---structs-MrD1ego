57c951092e23b545bfbd573a4ff180701a8f43ec
Your Name <you@example.com>
2024-03-15 14:47:47 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -26,10 +26,20 @@ Descripción: Escribe una función que tome un arreglo y su tamaño, y luego
 invierta el orden de sus elementos.
 */
 void reverseArray(int arr[], int size) {
-  for(int i = 0 ; i <= size; i++)
+  /*for(int i = 0 ; i <= size; i++)
     {
       arr[i] = arr[size -  1 - i];
       arr[size - 1 - i] = arr[i];
+    }*/
+  int temp;
+  int i = 0;
+  while(i < size)
+    {
+      temp = arr[i];
+      arr[i] = arr[size];
+      arr[size] = temp;
+      i++;
+      size--;
     }
 }
 
